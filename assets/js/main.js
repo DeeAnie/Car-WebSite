dfafb0ed145061639b7417e1350dae9fdd1ecb4c
Diana <diana.zhusupbekova.kk@gmail.com>
2022-03-28 21:03:58 +0600

    feat: add show menu JS

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+// SHOW MENU
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+//   MENU SHOW
+// Validate if constant exists | Проверить, существует ли константа
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+//   MENU HIDDEN
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+
+// REMOVE MENU MOBILE 
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== POPULAR SWIPER ===============*/
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+/* Link active featured */
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
