f0db6542fdb746f3bf256e87b7944bc0242a2533
Diana <diana.zhusupbekova.kk@gmail.com>
2022-03-29 00:14:27 +0600

    feat: Change Background Header JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,13 +23,21 @@ if (navClose) {
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-    const navMenu = document.getElementById("nav-menu")
-    // When we click on each nav__link, we remove the show-menu class | Когда мы нажимаем на каждую ссылку nav__, мы удаляем класс show-menu
-    navMenu.classList.remove("show-menu")
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class | Когда мы нажимаем на каждую ссылку nav__, мы удаляем класс show-menu
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+// CHANGE BACKGROUND HEADER
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag |
+  // Когда прокрутка превышает высоту окна просмотра 50, добавьте класс заголовка прокрутки в тег заголовка.
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== POPULAR SWIPER ===============*/
 
